5a3ef1ff6d51f983c55714d532390911ea78527a
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-02-09 14:38:25 -0500

    server.js
    I am adding code for the delete. I am trying to find out why it is not working.

--- server.js
+++ server.js
@@ -72,21 +72,20 @@ app.post("/api/notes", function (req, res) {
 });
 
 // Delete a note
-
-app.delete("/api/notes/:id", function(req, res) {
+app.delete("/api/notes/:id", function (req, res) {
   try {
     //  reads the json file
     notesData = fs.readFileSync("./db.json", "utf8");
     // parse the data to get an array of the objects
     notesData = JSON.parse(notesData);
     // delete the old note from the array on note objects
-    notesData = notesData.filter(function(note) {
+    notesData = notesData.filter(function (note) {
       return note.id != req.params.id;
     });
     // make it string(stringify)so you can write it to the file
     notesData = JSON.stringify(notesData);
     // write the new notes to the file
-    fs.writeFile("./db.json", notesData, "utf8", function(err) {
+    fs.writeFile("./db.json", notesData, "utf8", function (err) {
       // error handling
       if (err) throw err;
     });
